0442ff0baf75af9d146a0808365f809761ce2483
gra-nolan-bar <granolanbar@rocketmail.com>
2024-05-26 22:29:04 -0700

    completed challenge 01

---
+++ lesson03/calculator.js
@@ -0,0 +1,32 @@
+let i = 0; 
+let j = 1; 
+let k = 2;
+let l = 3;
+
+if (i <= 3) {
+  console.log('hello');
+}
+
+if (j < 1) {
+  console.log('goodbye')
+} else if (j = 1) {
+  console.log('continue' + ' ' + j)
+}
+
+for (k; k < 4; k++) {
+  console.log('continue' + ' ' + k)
+}
+
+while (l < 5) {
+  console.log('continue' + ' ' + (l + 1));
+  l++;
+}
+
+do {
+  console.log('continue' + ' ' + (l + 1));
+  i++;
+} while (i < 1);
+
+if (k = 4) {
+  console.log('goodbye');
+}
